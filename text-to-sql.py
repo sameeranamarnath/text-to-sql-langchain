0f012629ddf8891ae146fbedfb701b9693e0514f
amar.s <sameeranamarnath@gmail.com>
2023-11-24 18:26:51 +0530

    text-to-sql var using omdb database, added support for postgresql via psycopg2 with existing sqlite support

--- text-to-sql.py
+++ text-to-sql.py
@@ -6,7 +6,11 @@ from langchain_experimental.sql import SQLDatabaseChain
 
 
 def generate_response(input_text,database_uri="postgresql+psycopg2://default:GT4dg3OemzlA@ep-curly-violet-36509009.us-east-1.postgres.vercel-storage.com:5432/verceldb"):     
-     db = SQLDatabase.from_uri(database_uri)
+     if database_uri is None:
+      db = SQLDatabase.from_uri("postgresql+psycopg2://default:GT4dg3OemzlA@ep-curly-violet-36509009.us-east-1.postgres.vercel-storage.com:5432/verceldb")
+     else:
+      db = SQLDatabase.from_uri(database_uri)
+
      llm = OpenAI(temperature=0, verbose=True, openai_api_key=openai_api_key)
      db_chain = SQLDatabaseChain.from_llm(llm, db, verbose=True)
      st.info(db_chain.run(input_text))
