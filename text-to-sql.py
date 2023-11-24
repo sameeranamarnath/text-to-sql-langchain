59e1e872382f194ce7ec036636105d86e71f30bb
Sameeran Amarnath <sameeranamarnath@gmail.com>
2023-11-24 17:42:25 +0000

    model length eror fixed

--- text-to-sql.py
+++ text-to-sql.py
@@ -2,16 +2,16 @@ import streamlit as st
 from langchain.llms import OpenAI
 from langchain.utilities import SQLDatabase
 from langchain_experimental.sql import SQLDatabaseChain
-import psycopg2
 
 
 def generate_response(input_text,database_uri):     
      if not database_uri:
-      db = SQLDatabase.from_uri("postgresql+psycopg2://default:GT4dg3OemzlA@ep-curly-violet-36509009.us-east-1.postgres.vercel-storage.com:5432/verceldb")
+      #postgresql://sameeranamarnath:P6UClH5XpDdr@ep-floral-frog-97311990-pooler.ap-southeast-1.aws.neon.tech/omdb?sslmode=require
+      db = SQLDatabase.from_uri(f"postgresql+psycopg2://sameeranamarnath:P6UClH5XpDdr@ep-floral-frog-97311990-pooler.ap-southeast-1.aws.neon.tech/omdb?sslmode=require&options=endpoint%3Dep%2Dfloral%2Dfrog%2D97311990%2Dpooler")
      else:
       db = SQLDatabase.from_uri(database_uri)
 
-     llm = OpenAI(temperature=0, verbose=True, openai_api_key=openai_api_key)
+     llm = OpenAI(model="gpt-3.5-turbo-1106",  temperature=0, verbose=True, openai_api_key=openai_api_key,max_tokens=4096)
      db_chain = SQLDatabaseChain.from_llm(llm, db, verbose=True)
      st.info(db_chain.run(input_text))
 
